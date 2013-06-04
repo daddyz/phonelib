66b5ec6c4a58dd34cfbd3f1d5beb0836d7941f6f
Romain Laurent <romain.laurent@wopata.com>
2013-06-04 02:38:16 +0200

    Refactor Phonelib tests with shoulda-context

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+require 'shoulda-context'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
