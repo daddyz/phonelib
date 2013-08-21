18f6ad6d92ca77b1a21294a4f9e9fa0cd6780a51
Vadim Senderovich <vadims@netvertise.co.il>
2013-08-21 16:39:18 +0300

    put back comments for validator class

--- lib/validators/phone_validator.rb
+++ lib/validators/phone_validator.rb
@@ -1,3 +1,30 @@
+# Validator class for phone validations
+#
+# ==== Examples
+#
+# Validates that attribute is a valid phone number.
+# If empty value passed for attribute it fails.
+#
+#   class Phone < ActiveRecord::Base
+#     attr_accessible :number
+#     validates :number, phone: true
+#   end
+#
+# Validates that attribute is a possible phone number.
+# If empty value passed for attribute it fails.
+#
+#   class Phone < ActiveRecord::Base
+#     attr_accessible :number
+#     validates :number, phone: { possible: true }
+#   end
+#
+# Validates that attribute is a valid phone number.
+# Empty value is allowed to be passed.
+#
+#   class Phone < ActiveRecord::Base
+#     attr_accessible :number
+#     validates :number, phone: { allow_blank: true }
+#   end 
 class PhoneValidator < ActiveModel::EachValidator
   # Include all core methods
   include Phonelib::Core
