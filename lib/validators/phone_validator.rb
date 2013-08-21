34f973c0e4fbc3f255403013260f0c1f7951affc
ryancheung <ryancheung.go@gmail.com>
2013-08-21 15:05:36 +0800

    Change error message to be from translation

--- lib/validators/phone_validator.rb
+++ lib/validators/phone_validator.rb
@@ -8,8 +8,6 @@ class PhoneValidator < ActiveModel::EachValidator
     valid = options[:possible] ? phone.possible? : phone.valid?
     valid = true if options[:allow_blank] && phone.original.blank?
 
-    # TODO: change to be from translations
-    error = "is not valid"
-    record.errors.add(attribute, (options[:message] || error)) unless valid
+    record.errors.add(attribute, options[:message] || :invalid) unless valid
   end
 end
