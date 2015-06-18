5079e0d1ce06475d37e3fd2a90cede3c635b2923
Vadim Senderovich <vadims@commpeak.com>
2015-06-18 20:05:02 +0300

    more work on code climate

--- lib/phonelib/data_importer.rb
+++ lib/phonelib/data_importer.rb
@@ -1,3 +1,5 @@
+require 'phonelib/data_importer_helper'
+
 module Phonelib
   # module processes creation of data files needed for this gem
   module DataImporter
@@ -13,6 +15,8 @@ module Phonelib
 
     # class with functionality for importing data
     class Importer
+      include Phonelib::DataImporterHelper
+
       # main data file in repo
       MAIN_FILE = 'resources/PhoneNumberMetadata.xml'
       # short number metadata
@@ -26,8 +30,6 @@ module Phonelib
       # timezones data dir in repo
       TIMEZONES_DIR= 'resources/timezones/'
 
-      # xml comments attributes names that should not be parsed
-      XML_COMMENT_ATTRIBUTES = %w(text comment)
       # xml format attributes names
       XML_FORMAT_NAMES = %w(intlFormat format)
 
@@ -85,25 +87,6 @@ module Phonelib
         raise 'Could not clone repo' unless cloned
       end
 
-      # method extracts formats and types from xml data
-      def get_types_and_formats(children)
-        types = {}
-        formats = []
-
-        without_comments(children).each do | phone_type |
-          if phone_type.name == 'availableFormats'
-            formats = parse_formats(phone_type.children)
-          else
-            types[name2sym(phone_type.name)] =
-                get_hash_from_xml(phone_type, :element)
-          end
-        end
-
-        types = add_possible_if_not_exists(types)
-
-        { types: types, formats: formats }
-      end
-
       # method parses main data file
       def import_main_data
         puts 'IMPORTING MAIN DATA'
@@ -114,17 +97,6 @@ module Phonelib
 
           country.merge! get_types_and_formats(el.children)
           @data[country[:id]] = country
-        end
-      end
-
-      # method adds short number patterns to main data parsed from main xml
-      def merge_short_with_main_type(country_id, type, data)
-        data.each do |k, v|
-          if @data[country_id][:types][type][k]
-            @data[country_id][:types][type][k] += "|#{v}"
-          else
-            @data[country_id][:types][type][k] = v
-          end
         end
       end
 
@@ -189,6 +161,36 @@ module Phonelib
                               :c)
       end
 
+      # method extracts formats and types from xml data
+      def get_types_and_formats(children)
+        types = {}
+        formats = []
+
+        without_comments(children).each do | phone_type |
+          if phone_type.name == 'availableFormats'
+            formats = parse_formats(phone_type.children)
+          else
+            types[name2sym(phone_type.name)] =
+                get_hash_from_xml(phone_type, :element)
+          end
+        end
+
+        types = add_possible_if_not_exists(types)
+
+        { types: types, formats: formats }
+      end
+
+      # method adds short number patterns to main data parsed from main xml
+      def merge_short_with_main_type(country_id, type, data)
+        data.each do |k, v|
+          if @data[country_id][:types][type][k]
+            @data[country_id][:types][type][k] += "|#{v}"
+          else
+            @data[country_id][:types][type][k] = v
+          end
+        end
+      end
+
       # adds possible pattern in case it doesn't exists
       def add_possible_if_not_exists(types)
         types.each do |type, data|
@@ -198,33 +200,6 @@ module Phonelib
           end
         end
         types
-      end
-
-      # method filters xml elements excluding comments elements
-      def without_comments(data)
-        data.select do |el|
-          !XML_COMMENT_ATTRIBUTES.include? el.name
-        end
-      end
-
-      # method creates hash from xml elements/element attributes
-      def get_hash_from_xml(data, type)
-        hash = {}
-        case type
-          when :attributes
-            data.attributes.each do |k, v|
-              hash[name2sym(k)] = str_clean(v)
-            end
-          when :children
-            data.each do |f|
-              hash[name2sym(f[0])] = f[1]
-            end
-          when :element
-            data.elements.each do |child|
-              hash[name2sym(child.name)] = str_clean(child.children.first)
-            end
-        end
-        hash
       end
 
       # method parses xml for formats data
@@ -269,30 +244,9 @@ module Phonelib
         prefixes
       end
 
-      # method parses raw data file
-      def parse_raw_file(file)
-        data = {}
-        File.readlines(file).each do |line|
-          line = str_clean line
-          next if line.empty? || line[0] == '#'
-          prefix, line_data = line.split('|')
-          data[prefix] = line_data && line_data.split('&')
-        end
-        data
-      end
-
       # method for checking if element name is not a format element
       def is_not_format(name)
         !XML_FORMAT_NAMES.include? name
-      end
-
-      # get main body from parsed xml document
-      def main_from_xml(file)
-        xml_data = File.read(file)
-        xml_data.force_encoding("utf-8")
-
-        doc = Nokogiri::XML(xml_data)
-        doc.elements.first.elements.first
       end
 
       # method finds country by country prefix
@@ -303,21 +257,6 @@ module Phonelib
         end
 
         match.keys.first
-      end
-
-      # helper that cleans string
-      def str_clean(s, white_space = true)
-        s.to_s.tr(white_space ? " \n" : "\n", '')
-      end
-
-      # helper that converts xml element name to symbol
-      def name2sym(name)
-        camel2snake(name).to_sym
-      end
-
-      # method that converts camel case to snake case
-      def camel2snake(s)
-        s.gsub(/[A-Z]+/) { |m| "_#{m.downcase}" }
       end
     end
   end
---
+++ lib/phonelib/data_importer_helper.rb
@@ -0,0 +1,69 @@
+module Phonelib
+  module DataImporterHelper
+    # xml comments attributes names that should not be parsed
+    XML_COMMENT_ATTRIBUTES = %w(text comment)
+
+    # method filters xml elements excluding comments elements
+    def without_comments(data)
+      data.select do |el|
+        !XML_COMMENT_ATTRIBUTES.include? el.name
+      end
+    end
+
+    # method creates hash from xml elements/element attributes
+    def get_hash_from_xml(data, type)
+      hash = {}
+      case type
+        when :attributes
+          data.attributes.each do |k, v|
+            hash[name2sym(k)] = str_clean(v)
+          end
+        when :children
+          data.each do |f|
+            hash[name2sym(f[0])] = f[1]
+          end
+        when :element
+          data.elements.each do |child|
+            hash[name2sym(child.name)] = str_clean(child.children.first)
+          end
+      end
+      hash
+    end
+
+    # method parses raw data file
+    def parse_raw_file(file)
+      data = {}
+      File.readlines(file).each do |line|
+        line = str_clean line
+        next if line.empty? || line[0] == '#'
+        prefix, line_data = line.split('|')
+        data[prefix] = line_data && line_data.split('&')
+      end
+      data
+    end
+
+    # get main body from parsed xml document
+    def main_from_xml(file)
+      xml_data = File.read(file)
+      xml_data.force_encoding("utf-8")
+
+      doc = Nokogiri::XML(xml_data)
+      doc.elements.first.elements.first
+    end
+
+    # helper that cleans string
+    def str_clean(s, white_space = true)
+      s.to_s.tr(white_space ? " \n" : "\n", '')
+    end
+
+    # helper that converts xml element name to symbol
+    def name2sym(name)
+      camel2snake(name).to_sym
+    end
+
+    # method that converts camel case to snake case
+    def camel2snake(s)
+      s.gsub(/[A-Z]+/) { |m| "_#{m.downcase}" }
+    end
+  end
+end
