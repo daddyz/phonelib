2411beac43743581b53307a535bad3a7a451a4f4
Vadim Senderovich <vadims@commpeak.com>
2018-09-13 09:43:07 +0300

    fixing sqlite3 test failure

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20180731040557) do
 
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "ar_internal_metadata", ["key"], name: "sqlite_autoindex_ar_internal_metadata_1", unique: true
-
   create_table "phones", force: :cascade do |t|
     t.string   "number"
     t.datetime "created_at"
