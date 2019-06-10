c45de9d9e91f1819f3335fd0695d246e32c76fd9
Vadim Senderovich <vadims@commpeak.com>
2019-06-10 07:28:52 +0300

    added cuba for allowed double prefix countries

--- lib/phonelib/data_importer.rb
+++ lib/phonelib/data_importer.rb
@@ -18,7 +18,7 @@ module Phonelib
       include Phonelib::DataImporterHelper
 
       # countries that can have double country prefix in number
-      DOUBLE_COUNTRY_CODES_COUNTRIES = %w(IN DE BR IT NO PL)
+      DOUBLE_COUNTRY_CODES_COUNTRIES = %w(IN DE BR IT NO PL CU)
 
       # main data file in repo
       MAIN_FILE = 'resources/PhoneNumberMetadata.xml'
