9b4f70b54830cfd5e8332500bf2adb5bf935713d
Khaled AbuShqear <qmax93@gmail.com>
2021-08-25 01:46:45 +0300

    Update validation: phone valid if one country is valid

--- lib/validators/phone_validator.rb
+++ lib/validators/phone_validator.rb
@@ -60,7 +60,8 @@ class PhoneValidator < ActiveModel::EachValidator
     return if options[:allow_blank] && value.blank?
 
     @phone = parse(value, specified_country(record))
-    valid = phone_valid? && valid_types? && valid_country? && valid_extensions?
+    valid = countries.present? ? valid_country? : phone_valid?
+    valid = valid && valid_types? && valid_extensions?
 
     record.errors.add(attribute, message, options) unless valid
   end
@@ -82,7 +83,7 @@ class PhoneValidator < ActiveModel::EachValidator
 
   def valid_country?
     return true unless options[:countries]
-    (phone_countries & countries).size > 0
+    countries.any? { |country| @phone.valid_for_country?(country) }
   end
 
   def valid_extensions?
