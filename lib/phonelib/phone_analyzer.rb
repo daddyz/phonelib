13d0621d413d04d5095bf6c0de0b3dfe6a1857ef
Mitchell Henke <mitchell.henke@gsa.gov>
2025-02-07 12:20:43 -0600

    Use MatchData indexing instead of creating array

--- lib/phonelib/phone_analyzer.rb
+++ lib/phonelib/phone_analyzer.rb
@@ -185,7 +185,7 @@ module Phonelib
       if Phonelib.additional_regexes.is_a?(Hash) && Phonelib.additional_regexes[data[:id]]
         index = Phonelib.additional_regexes[data[:id]].values.flatten.join('|').scan(/\(/).size
       end
-      phone = country_match.to_a[-1 - index]
+      phone = country_match[-1 - index]
       result[:national] = phone
       result[:format] = number_format(phone, data[Core::FORMATS])
       result.merge! all_number_types(phone, data[Core::TYPES], not_valid)
